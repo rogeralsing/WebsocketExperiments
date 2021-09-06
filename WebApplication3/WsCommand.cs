e91ac00570b35cbc6df3d4cff72483d4bf1dcb0e
Roger Johansson <roger.johansson@charge-amps.com>
2021-09-06 11:28:17 +0200

    .

--- WebApplication3/WsCommand.cs
+++ WebApplication3/WsCommand.cs
@@ -6,13 +6,13 @@ namespace WebApplication3
     public class WsCommand
     {
         private readonly string _id;
-        private readonly WsConnection _socket;
+        private readonly WsConnection _connection;
         private readonly TaskCompletionSource _tcs = new();
 
-        public WsCommand(string id, WsConnection socket)
+        public WsCommand(string id, WsConnection connection)
         {
             _id = id;
-            _socket = socket;
+            _connection = connection;
         }
         public async Task StartAsync()
         {
@@ -23,19 +23,26 @@ namespace WebApplication3
 
             if (_tcs.Task.IsCompleted)
             {
-                //done
-                //done
-                //do stuff
-                Console.WriteLine($"Command {_id} completed");
+                await OnCompletedAsync();
             }
             else
             {
-                //timeout
-                //do stuff
-                Console.WriteLine($"Command {_id} timed out");
+                await OnTimeoutAsync();
             }
             
-            _socket.UnregisterCommand(_id);
+            _connection.UnregisterCommand(_id);
+        }
+
+        private async Task OnTimeoutAsync()
+        {
+            //timeout
+            Console.WriteLine($"Command {_id} timed out");
+        }
+
+        private async Task OnCompletedAsync()
+        {
+            //completed
+            Console.WriteLine($"Command {_id} completed");
         }
 
         public void Complete()
